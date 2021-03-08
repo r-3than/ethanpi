609bed69982fea699b105f1264c00d46d6ebaa1a
r-3than <ethan.gregory.ray@gmail.com>
2021-03-08 18:40:31 +0000

    bluetooth changes

--- web/webhost.py
+++ web/webhost.py
@@ -4,13 +4,24 @@ import threading
 import time, platform
 import datetime
 import ctypes, sys
-import string , random
+import string , random , bluetooth
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    f = ["test"]
-    return render_template("index.html",webpages=f)
+    bluetooth_devices = bluetooth.discover_devices(lookup_names = True, lookup_class = True)
+
+    number_of_devices = len(bluetooth_devices)
+
+    print(number_of_devices,"devices found")
+    addresses = []
+    names = []
+    device_classes = []
+    for addr, name, device_class in bluetooth_devices:
+        addresses.append(addr)
+        names.append(name)
+        device_classes.append(device_class)
+    return render_template("index.html",devices=names)
 
 app.run(port=5002,debug=False,host="0.0.0.0")
