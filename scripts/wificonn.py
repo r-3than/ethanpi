269bd4a241e4ed1c6970057285add625b40701c7
r-3than <ethan.gregory.ray@gmail.com>
2021-03-11 00:49:23 +0000

    ReaperConn

--- scripts/wificonn.py
+++ scripts/wificonn.py
@@ -1,4 +1,8 @@
 from wifi import Cell, Scheme
 print(Cell.all('wlan0'))
 for obj in Cell.all('wlan0'):
-    print(obj)
+    if obj.ssid == "REAPER":
+        scheme = Scheme.for_cell('wlan0', 'home', obj, "4f3cf593aa")
+        scheme.save()
+        scheme.activate()
+
